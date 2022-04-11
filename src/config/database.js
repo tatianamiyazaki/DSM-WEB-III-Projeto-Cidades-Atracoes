879accfdb03a7fdb6eedff3d99a06dfb68dac86d
tatianamiyazaki <tatimiyazaki@gmail.com>
2022-04-11 13:38:18 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     host: "localhost",
     dialect: "mysql",
     username: "root",
-    password: "Th167575#",
+    password: "Senha123",
     database: "cidades",
     define: {
         timestamps: true,
